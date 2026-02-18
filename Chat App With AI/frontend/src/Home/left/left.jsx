c9c2d264804d73c8d84f83476cb31edab2b5263c
yashrajyadav2002 <yyash9631@gmail.com>
2026-02-18 23:42:31 +0530

    17feb

---
+++ Chat App With AI/frontend/src/Home/left/left.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Left = () => {
+  return (
+    <>
+
+   <div className="border border-black p-4">
+      Left
+    </div>
+    </>
+  )
+};
+
+export default Left;
