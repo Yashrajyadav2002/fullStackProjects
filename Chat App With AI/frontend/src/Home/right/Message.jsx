14547ba77cc2e4d97d6f4c9fdd4e68e0820535f6
yashrajyadav2002 <yyash9631@gmail.com>
2026-02-25 23:16:55 +0530

    slow

--- Chat App With AI/frontend/src/Home/right/Message.jsx
+++ Chat App With AI/frontend/src/Home/right/Message.jsx
@@ -9,6 +9,8 @@ function Message() {
           <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
         </div>
 
+        <h1>This is the chat section </h1>
+
         <div className="chat chat-end">
           <div className="chat-bubble chat-bubble-accent">
             That's never been done in the history of the Jedi.
